2240bdc41961b313e7b44acb546670803d7ba75d
Rusakova Veronika <v.rusakova@MAC276RDS.Dlink>
2020-05-07 01:32:04 +0300

    вроде пофикшены 3 кейса. нужны правки по 4му

--- Coursera_python/wikistat.py
+++ Coursera_python/wikistat.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import unittest
+import re
 
 
 def num_1_image(body):
@@ -9,9 +10,12 @@ def num_1_image(body):
     img_tags = body.findAll(name='img')
     num_of_img = 0
     for tag in img_tags:
-        width = tag['width']
-        if int(width) >= 200:
-            num_of_img += 1
+        try:
+            width = tag['width']
+            if int(width) >= 200:
+                num_of_img += 1
+        except:
+            pass
     return num_of_img
 
 
@@ -23,7 +27,6 @@ def num_2_headers(body):
     h_tags = body.findAll(('h1', 'h2', 'h3', 'h4', 'h5', 'h6'))
     num_of_tags = 0
     for tag in h_tags:
-        # print(tag)
         if tag.text[0] == 'E' \
                 or tag.text[0] == 'T' \
                 or tag.text[0] == 'C':
@@ -36,15 +39,46 @@ def num_3_links(body):
     закрывающихся. Например: <p><span><a></a></span>, <a></a>, <a></a></p> - тут 2 ссылки подряд, т.к. закрывающийся
     span прерывает последовательность. <p><a><span></span></a>, <a></a>, <a></a></p> - а тут 3 ссылки подряд,
     т.к. span находится внутри ссылки, а не между ссылками. """
+    all_a = body.find_all('a')
+    nums = [1, ]
 
-    pass
+    while all_a:
+        a = all_a.pop()
+        i = 1
+        for sib in a.find_next_siblings():
+            if sib.name == 'a':
+                i += 1
+            else:
+                if nums[-1] < i:
+                    nums.append(i)
+                i = 0
+            if nums[-1] < i:
+                nums.append(i)
+
+    # print(nums)
+    max_num = max(nums)
+    # print(f"max={max_num}")
+    return max_num
 
 
 def num_4_lists(body):
     """Количество списков (ul, ol), не вложенных в другие списки. Например: <ol><li></li></ol>,
     <ul><li><ol><li></li></ol></li></ul> - два не вложенных списка (и один вложенный) """
-
-    pass
+    ul_ol_tags = body.findAll(('ul', 'ol'))
+    independent_tags = 0
+    for tag in ul_ol_tags:
+        flag = 0
+        try:
+            parents = tag.findParents()
+            for parent in parents:
+                parent_tag = parent.name
+                if parent_tag == 'ol' or parent_tag == 'ul':# or parent_tag == 'li':
+                    flag = 1
+            if flag == 0:
+                independent_tags += 1
+        except:
+            pass
+    return independent_tags
 
 
 def parse(path_to_file):
@@ -56,23 +90,24 @@ def parse(path_to_file):
         headers = num_2_headers(body)
         linkslen = num_3_links(body)
         lists = num_4_lists(body)
-
-        pass
-    # Поместите ваш код здесь.
-    # ВАЖНО!!!
-    # При открытии файла, добавьте в функцию open необязательный параметр
-    # encoding='utf-8', его отсутствие в коде будет вызвать падение вашего
-    # решения на грейдере с ошибкой UnicodeDecodeError
+        print([imgs, headers, linkslen, lists], path_to_file)
     return [imgs, headers, linkslen, lists]
 
 
 class TestParse(unittest.TestCase):
+    # def test_parse(self):
+    #     test_cases = (
+    #         ('wiki/608_(number)', [0, 1, 12, 120]),
+    #         ('wiki/Spectrogram', [1, 2, 4, 7]),)
+
     def test_parse(self):
         test_cases = (
             ('wiki/Stone_Age', [13, 10, 12, 40]),
             ('wiki/Brain', [19, 5, 25, 11]),
             ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
             ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
+            ('wiki/Social_theory', [0, 8, 12, 10]),
+            ('wiki/608_(number)', [0, 1, 12, 120]),
             ('wiki/Spectrogram', [1, 2, 4, 7]),)
 
         for path, expected in test_cases:
